67f54218bbec8e6d0d7d039d34a25e05129dd6f0
trapp <rapp@equinoxe.info>
2011-03-30 11:30:55 +0200

    Arrays with multiple values can be declared on one line.

--- Sniffs/Arrays/ArrayDeclarationSniff.php
+++ Sniffs/Arrays/ArrayDeclarationSniff.php
@@ -179,11 +179,11 @@ class eqCodingGuideline_Sniffs_Arrays_ArrayDeclarationSniff implements PHP_CodeS
             if ($valueCount > 0) {
                 $conditionCheck = $phpcsFile->findPrevious(array(T_OPEN_PARENTHESIS, T_SEMICOLON), ($stackPtr - 1), null, false);
 
-                if (($conditionCheck === false) || ($tokens[$conditionCheck]['line'] !== $tokens[$stackPtr]['line'])) {
+                /*if (($conditionCheck === false) || ($tokens[$conditionCheck]['line'] !== $tokens[$stackPtr]['line'])) {
                     $error = 'Array with multiple values cannot be declared on a single line';
                     $phpcsFile->addError($error, $stackPtr, 'SingleLineNotAllowed');
                     return;
-                }
+                }*/
 
                 // We have a multiple value array that is inside a condition or
                 // function. Check its spacing is correct.
